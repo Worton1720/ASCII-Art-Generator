af10bdec9d34e613deca8775365fc12d9974e11b
Worton <124402406+Worton1720@users.noreply.github.com>
2024-01-22 12:53:13 +0300

    Add files via upload

--- ASCIIArtGenerator.py
+++ ASCIIArtGenerator.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 import os
 import imageio
 from PIL import Image, ImageDraw, ImageFont
+from tqdm import tqdm
 
 
 # класс для генерации ASCII-арт из изображений и GIF
@@ -23,10 +24,11 @@ class ASCIIArtGenerator:
         self._path_file = None
 
     # преобразование GIF в набор PNG
-    def gif_to_png(self, gif_path: str) -> List[Path]:
+    def gif_to_png(self, gif_path: str, show_progress_bar: bool = False) -> List[Path]:
         """
         Преобразование GIF-анимации в отдельные PNG-изображения.
         :param gif_path: путь к GIF-файлу
+        :param show_progress_bar: отображение прогресс-бара
         :return: директория, в которой сохранены PNG-изображения
         """
         # Получение имени файла без расширения
@@ -45,80 +47,102 @@ class ASCIIArtGenerator:
         png_dir = os.path.join(gif_dir, f"{base_name}")
         os.makedirs(png_dir, exist_ok=True)
 
+        total_frames = len(images)
+
         # Сохранение PNG-файлов в папку
-        for i, image in enumerate(images):
-            png_path = os.path.join(png_dir, f"{base_name}_frame_{i+1}.png")
-            image.save(png_path)
+        # Добавляем прогресс-бар для преобразования каждого кадра в PNG
+        with tqdm(
+            total=total_frames,
+            desc="Converting frames to PNG",
+            disable=not show_progress_bar,
+        ) as pbar:
+            for i, image in enumerate(images):
+                png_path = os.path.join(png_dir, f"{base_name}_frame_{i+1}.png")
+                image.save(png_path)
+                pbar.update(1)
 
         return png_dir
 
-    def gif_to_ascii(self, gif_path: str) -> List[str]:
+    def gif_to_ascii(self, gif_path: str, show_progress_bar: bool = False) -> List[str]:
         """
         Преобразование GIF-анимации в ASCII-арт.
         :param gif_path: путь к GIF-файлу
+        :param show_progress_bar: отображение прогресс-бара
         :return: список строк с ASCII-арт для каждого кадра
         """
-
-        # Инициализация списка для хранения Unicode-представления gif
         unicode_frames = []
 
         # Открываем GIF-файл с помощью Pillow
         with Image.open(gif_path) as gif:
-            frames = []
+            total_frames = gif.n_frames
 
-            # Итерируем по всем кадрам GIF-файла
-            for i in range(gif.n_frames):
-                gif.seek(i)
+            # Добавляем прогресс-бар для итерации по кадрам GIF-файла
+            with tqdm(
+                total=total_frames,
+                desc="Converting GIF frames to ASCII",
+                disable=not show_progress_bar,
+            ) as pbar:
+                for i in range(total_frames):
+                    gif.seek(i)
 
-                # Преобразование изображения в объект PIL
-                img = gif.convert("RGBA")
+                    # Преобразование изображения в объект PIL
+                    img = gif.convert("RGBA")
 
-                # Изменение размера изображения
-                width, height = img.size
-                img = img.resize(
-                    (
-                        int(self.__scale_factor * width),
-                        int(self.__scale_factor * height),
-                    )
-                )
-
-                # Преобразование в оттенки серого
-                pixels = img.convert("L")
-
-                # Преобразование пикселей в символы Unicode
-                ascii_str = ""
-                for pixel_value in pixels.getdata():
-                    # Используем min и max, чтобы гарантировать, что индекс находится в пределах допустимых значений
-                    index = max(
-                        0,
-                        min(
-                            pixel_value // (256 // len(self.__ascii_chars)),
-                            len(self.__ascii_chars) - 1,
-                        ),
-                    )
-                    ascii_str += self.__ascii_chars[index]
-
-                # Разделение текста на строки для отображения
-                ascii_str_len = len(ascii_str)
-                img_ascii = ""
-                for j in range(0, ascii_str_len, int(self.__scale_factor * width)):
-                    img_ascii += (
-                        ascii_str[j : j + int(self.__scale_factor * width)] + "\n"
+                    # Изменение размера изображения
+                    width, height = img.size
+                    img = img.resize(
+                        (
+                            int(self.__scale_factor * width),
+                            int(self.__scale_factor * height),
+                        )
                     )
 
-                # Добавление строки в список
-                unicode_frames.append(img_ascii + "\\f")
+                    # Преобразование в оттенки серого
+                    pixels = img.convert("L")
+
+                    # Преобразование пикселей в символы Unicode
+                    ascii_str = ""
+                    for pixel_value in pixels.getdata():
+                        # Используем min и max, чтобы гарантировать, что индекс находится в пределах допустимых значений
+                        index = max(
+                            0,
+                            min(
+                                pixel_value // (256 // len(self.__ascii_chars)),
+                                len(self.__ascii_chars) - 1,
+                            ),
+                        )
+                        ascii_str += self.__ascii_chars[index]
+
+                    # Разделение текста на строки для отображения
+                    ascii_str_len = len(ascii_str)
+                    img_ascii = ""
+                    for j in range(0, ascii_str_len, int(self.__scale_factor * width)):
+                        img_ascii += (
+                            ascii_str[j : j + int(self.__scale_factor * width)] + "\n"
+                        )
+
+                    # Добавление строки в список
+                    unicode_frames.append(img_ascii + "\\f")
+
+                    # Обновляем прогресс-бар
+                    pbar.update(1)
 
         self.__frames = unicode_frames
         self._path_file = gif_path
         return unicode_frames
 
     # преобразование GIF или изображения в ASCII-арт
-    def image_to_ascii(self, image_path: str, output_file: str = None) -> str:
+    def image_to_ascii(
+        self,
+        image_path: str,
+        output_file: str = None,
+        show_progress_bar: bool = False,
+    ) -> str:
         """
         Преобразование изображения в ASCII-арт.
         :param image_path: путь к изображению
         :param output_file: путь к файлу, в который будет сохранен ASCII-арт (по умолчанию, тот же путь, что и изображение)
+        :param show_progress_bar: отображение прогресс-бара
         :return: строка с ASCII-арт
         """
 
@@ -136,6 +160,7 @@ class ASCIIArtGenerator:
 
         # Преобразование пикселей в символы Unicode
         ascii_str = ""
+
         for pixel_value in pixels.getdata():
             # Используем min и max, чтобы гарантировать, что индекс находится в пределах допустимых значений
             index = max(
@@ -150,8 +175,17 @@ class ASCIIArtGenerator:
         # Разделение текста на строки для отображения
         ascii_str_len = len(ascii_str)
         img_ascii = ""
-        for j in range(0, ascii_str_len, int(self.__scale_factor * width)):
-            img_ascii += ascii_str[j : j + int(self.__scale_factor * width)] + "\n"
+
+        total_lines = int(ascii_str_len / (self.__scale_factor * width))
+        # Добавляем прогресс-бар для формирования каждой строки ASCII-арта
+        with tqdm(
+            total=total_lines,
+            desc="Generating ASCII lines",
+            disable=not show_progress_bar,
+        ) as pbar:
+            for j in range(0, ascii_str_len, int(self.__scale_factor * width)):
+                img_ascii += ascii_str[j : j + int(self.__scale_factor * width)] + "\n"
+                pbar.update(1)
 
         self.__frames = [img_ascii]
         self._path_file = image_path
@@ -165,6 +199,7 @@ class ASCIIArtGenerator:
         font_path: str = "consola.ttf",
         output_file: str = None,
         save_factor: bool = True,
+        show_progress_bar: bool = False,
     ) -> Image:
         """
         Преобразование ASCII-арт в изображение.
@@ -174,6 +209,7 @@ class ASCIIArtGenerator:
         :param output_file: путь к файлу, в который будет сохранен ASCII-арт (по умолчанию, будет создан рядом с исходным файлом)
         :return: объект PIL.Image
         :param save_factor: сохранить ли в виде файла
+        :param show_progress_bar: отображение прогресс-бара
         """
 
         # Разбиваем ASCII-арт по символам новой строки
@@ -188,11 +224,22 @@ class ASCIIArtGenerator:
         draw = ImageDraw.Draw(img)
 
         # Загрузка шрифта
-        font = ImageFont.truetype(font_path, font_size*1.8)
+        font = ImageFont.truetype(font_path, font_size * 1.8)
 
         # Отрисовка ASCII-арт на изображении
-        for i, line in enumerate(lines):
-            draw.text((0, i * font_size), line, font=font, fill="black")
+        total_frames = len(lines)
+
+        # Используем tqdm для отображения прогресса
+        with tqdm(
+            total=total_frames,
+            desc="Converting ASCII lines to Image",
+            disable=not show_progress_bar,
+        ) as pbar:
+            for i, line in enumerate(lines):
+                draw.text((0, i * font_size), line, font=font, fill="black")
+
+                # Обновляем прогресс-бар
+                pbar.update(1)
 
         if output_file is None:
             # Получение имени файла без расширения из path_file
@@ -218,6 +265,7 @@ class ASCIIArtGenerator:
         font_path: str = "consola.ttf",
         duration: float = 1,
         save_factor: bool = False,
+        show_progress_bar: bool = False,
     ) -> Image:
         """
         Преобразование ASCII-арт в GIF-анимацию.
@@ -227,14 +275,26 @@ class ASCIIArtGenerator:
         :param duration: длительность каждого кадра в милисекундах
         :param font_path: путь к файлу шрифта
         :param save_factor: сохранить ли в виде файла
+        :param show_progress_bar: отображение прогресс-бара
         """
         images = []
 
-        for frame in ascii_frames:
-            img = self.ascii_to_image(
-                frame, font_size, font_path, save_factor=save_factor
-            )
-            images.append(img)
+        total_frames = len(ascii_frames)
+
+        # Используем tqdm для отображения прогресса
+        with tqdm(
+            total=total_frames,
+            desc="Converting ASCII frames to GIF",
+            disable=not show_progress_bar,
+        ) as pbar:
+            for frame in ascii_frames:
+                img = self.ascii_to_image(
+                    frame, font_size, font_path, save_factor=save_factor
+                )
+                images.append(img)
+
+                # Обновляем прогресс-бар
+                pbar.update(1)
 
         if output_file == None:
             # Получение имени файла без расширения из path_file
--- main.py
+++ main.py
@@ -1,27 +1,70 @@
 import ASCIIArtGenerator
 from tkinter import Tk, filedialog
 
-def get_file_path():
+
+def get_file_paths():
     root = Tk()
     root.withdraw()
-    file_path = filedialog.askopenfilename(title="Выберите файл")
-    if file_path == "":
-        print("Invalid file path")
-        exit(1)
-    print("File path:", file_path)
-    return file_path
+    file_paths_tuple = filedialog.askopenfilenames(
+        title="Выберите файлы",
+        filetypes=[
+            ("All Files", "*.*"),
+            ("Image files", "*.png;*.jpg"),
+            ("Gif files", "*.gif"),
+        ],
+    )
 
-def process_image_conversion(file_path, scale_factor, font_size, save_option, ascii_generator):
-    ascii_generator.ascii_to_image(ascii_generator.image_to_ascii(file_path), font_size=font_size)
+    if not file_paths_tuple:
+        print("No files selected")
+        exit(1)
+
+    # Если выбран только один файл, преобразуем кортеж в список
+    file_paths = (
+        list(file_paths_tuple)
+        if isinstance(file_paths_tuple, tuple)
+        else file_paths_tuple
+    )
+
+    print("Selected file paths:")
+    for file_path in file_paths:
+        print(file_path)
+
+    return file_paths
+
+
+def process_image_conversion(
+    file_path: str,
+    ascii_generator: ASCIIArtGenerator.ASCIIArtGenerator,
+    font_size: int = 7,
+    save_option: bool = False,
+):
+    ascii_generator.ascii_to_image(
+        ascii_generator.image_to_ascii(image_path=file_path, show_progress_bar=True),
+        font_size=font_size,
+        show_progress_bar=True,
+    )
     if save_option:
         ascii_generator.save_to_file()
 
-def process_gif_conversion(file_path, scale_factor, font_size, save_option, speed_gif, ascii_generator):
+
+def process_gif_conversion(
+    file_path: str,
+    ascii_generator: ASCIIArtGenerator.ASCIIArtGenerator,
+    font_size: int = 7,
+    save_option: bool = False,
+    speed_gif: int = 1,
+):
     if speed_gif is None:
         speed_gif = float(input("Speed of gif (milliseconds): ") or 1)
-    ascii_generator.ascii_to_gif(ascii_generator.gif_to_ascii(file_path), font_size=font_size, duration=speed_gif)
+    ascii_generator.ascii_to_gif(
+        ascii_generator.gif_to_ascii(gif_path=file_path, show_progress_bar=True),
+        font_size=font_size,
+        duration=speed_gif,
+        show_progress_bar=True,
+    )
     if save_option:
         ascii_generator.save_to_file()
+
 
 def main():
     print("1 - img to ascii\n2 - gif")
@@ -30,27 +73,46 @@ def main():
         print("Invalid option")
         exit(1)
 
-    file_path = get_file_path()
+    file_paths = get_file_paths()
     ascii_generator = ASCIIArtGenerator.ASCIIArtGenerator()
 
     if file_type == 2:
         print("y - gif to png \nor gif to ascii")
         sub_choice = input("> ").lower()
         if sub_choice == "y":
-            ascii_generator.gif_to_png(file_path)
+            ascii_generator.gif_to_png(gif_path=file_paths, show_progress_bar=True)
             print("Conversion complete.")
             exit(0)
 
     scale_factor = int(input("Enter the scale factor: ")) or 100
     font_size = int(input("Enter the font size: ")) or 5
-    save_option = str(input("Save the ascii animation to a file? (y/n): ").lower()) == "y"
+    save_option = (
+        str(input("Save the ascii animation to a file? (y/n): ").lower()) == "y"
+    )
 
-    ascii_generator = ASCIIArtGenerator.ASCIIArtGenerator(scale_factor)
-    
-    if file_type == 1:
-        process_image_conversion(file_path, scale_factor, font_size, save_option, ascii_generator)
-    elif file_type == 2:
-        process_gif_conversion(file_path, scale_factor, font_size, save_option, None, ascii_generator)
+    ascii_generator = ASCIIArtGenerator.ASCIIArtGenerator(scale_factor=scale_factor)
+
+    for i, file_path in enumerate(file_paths):
+        if len(file_paths) == 1:
+            print(f"image generated")
+        else:
+            print(f"image {i+1}/{len(file_paths)}: ")
+        if file_type == 1:
+            process_image_conversion(
+                file_path=file_path,
+                font_size=font_size,
+                save_option=save_option,
+                ascii_generator=ascii_generator,
+            )
+        elif file_type == 2:
+            process_gif_conversion(
+                file_path=file_path,
+                font_size=font_size,
+                save_option=save_option,
+                ascii_generator=ascii_generator,
+                speed_gif=1,
+            )
+
 
 if __name__ == "__main__":
     main()
